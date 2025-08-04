3102f3460c4a8d80be4526673ec06f5b3159828b
Sakshay Mahna <sakshum19@gmail.com>
2025-08-04 10:51:02 +0530

    Support use case #39

---
+++ parser/parser/handlers/set_environment_variable_handler.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2025 Kodo Robotics
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import ast
+from parser.context import ParseContext
+from parser.parser.registry import register_handler
+from parser.resolution.utils import resolve_call_signature
+
+
+@register_handler("SetEnvironmentVariable", "launch.actions.SetEnvironmentVariable")
+def handle_set_environment_variable(node: ast.Call, context: ParseContext) -> dict:
+    args, kwargs = resolve_call_signature(node, context.engine)
+    
+    # Handle both positional and keyword style
+    if "name" not in kwargs and args:
+        kwargs["name"] = args[0]
+    if "value" not in kwargs and args:
+        kwargs["value"] = args[1]
+
+    # Track environment variable usage
+    context.introspection.track_environment_variable(kwargs["name"], kwargs)
+
+    return {"type": "EnvironmentVariable", **kwargs}
